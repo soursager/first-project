e7972a051c9e3aaf3647ffb5baf397b1cacfe29a
Игорь <budasovigor@yandex.ru>
2023-05-29 22:54:47 +0300

    Создал программу високосного года в Praktikum

--- Praktikum.java
+++ Praktikum.java
@@ -1,1 +1,30 @@
-public static void main(Strring[] args) {
+import java.util.Scanner;
+
+public class Praktikum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите год:");
+        int year = scanner.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println("12.09." + year);
+        } else {
+            System.out.println("13.09." + year);
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 100 == 0) {
+            return false;
+        } else if (year % 4 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+
+
